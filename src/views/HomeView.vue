c49f6daa988e9e43e790773d81251931f8f9c098
Victor Degaspari <vdegaspari.vm@gmail.com>
2022-05-24 12:13:50 -0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script>
+import TheWelcome from "@/components/TheWelcome.vue";
+export default {
+  components: {
+    TheWelcome,
+  },
+};
+</script>
